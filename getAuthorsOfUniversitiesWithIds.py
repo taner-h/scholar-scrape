409da49b2d31a62745e45e2cfe38053eda849d19
Taner Hacıoğlu <tanerfb1999@gmail.com>
2023-04-27 15:28:22 +0300

    current page is now printed

--- getAuthorsOfUniversitiesWithIds.py
+++ getAuthorsOfUniversitiesWithIds.py
@@ -1,4 +1,5 @@
 import json
+import sys
 from scholarly import scholarly
 
 with open("universities.json", "r", encoding="utf-8") as f:
@@ -6,8 +7,8 @@ with open("universities.json", "r", encoding="utf-8") as f:
 
 data = [uni for uni in data if uni['id']]
 
-START = 50  # Dahil
-END = 75  # Haric
+START = 50      # Dahil
+END = 70        # Haric
 
 uniCount = 1
 for uni in data[START:END]:
@@ -16,11 +17,22 @@ for uni in data[START:END]:
     print(f'\n{name} ({uniCount}/{END-START})')
 
     count = 1
+    page = 1
+    print('page: ', end='')
+    sys.stdout.flush()
+
     results = []
     query = scholarly.search_author_by_organization(id)
+
     for result in query:
         results.append(result)
         count += 1
+
+        if (count // 10) + 1 > page:
+            print('X', end='')
+            sys.stdout.flush()
+
+        page = (count // 10) + 1
 
     print(f'found {count} authors')
     uniCount += 1
