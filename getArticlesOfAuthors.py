0bac3d5c169bb2d301e10fad6bd6fbd1be52e5f7
Taner Hacıoğlu <tanerfb1999@gmail.com>
2023-04-28 17:58:20 +0300

    added author data & getAuthor.py

---
+++ getArticlesOfAuthors.py
@@ -0,0 +1,63 @@
+import json
+import time
+import copy
+from scholarly import scholarly
+
+SLEEP = 1
+START = 0
+END = 10
+
+with open("universities.json", "r", encoding="utf-8") as f:
+    data = json.load(f)
+
+data = [uni for uni in data if uni['id']]
+
+total = 1
+uniCount = 1
+
+for uni in data[START:END]:
+    name = uni['name']
+
+    print(f'\n{name} ({uniCount}/{END-START})')
+
+    with open(f"./data/{name}.json", "r", encoding="utf-8") as f:
+        universityData = json.load(f)
+
+    uniData = copy.deepcopy(universityData)
+
+    for index, author in enumerate(uniData):
+        authorId = author.get('scholar_id', None)
+        if not authorId:
+            continue
+
+        isAuthorFilled = "publications" in author.get('filled', [])
+        if isAuthorFilled:
+            continue
+
+        time.sleep(SLEEP)
+
+        time1 = time.time()
+
+        authorUnfilled = scholarly.search_author_id(authorId)
+        authorFilled = scholarly.fill(
+            authorUnfilled, sections=['publications'])
+
+        time2 = time.time()
+
+        articleCount = len(authorFilled.get('publications', []))
+        timeSpent = round(time2 - time1, 2)
+
+        print(
+            f"found author with {articleCount} articles in {timeSpent} seconds ({total})")
+
+        universityData[index] = authorFilled
+
+        with open(f'./data/{name}.json', 'w', encoding='utf-8') as f:
+            json.dump(universityData, f, ensure_ascii=False, indent=4)
+
+        total += 1
+
+    with open(f'./data/{name}.json', 'w', encoding='utf-8') as f:
+        json.dump(universityData, f, ensure_ascii=False, indent=4)
+
+    uniCount += 1
---
+++ getNumberOfAuthors.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+
+let count = 0;
+
+const data = JSON.parse(fs.readFileSync("universities.json", "utf8"));
+const universities = data.map((uni) => ({ ...uni, authorCount: null }));
+console.log("universities", universities);
+
+fs.readdirSync("./scholar/").forEach((file, index) => {
+  if (file !== ".gitignore") {
+    const json = JSON.parse(fs.readFileSync("./scholar/" + file, "utf8"));
+    count += json.length;
+    universities.find(
+      (uni) => uni.name.localeCompare(file.split(".")[0]) === 0
+    ).authorCount = json.length;
+    console.log(index, ": ", json.length);
+  }
+});
+
+fs.writeFileSync("universities2.json", JSON.stringify(universities));
+
+console.log("total: ", count);
